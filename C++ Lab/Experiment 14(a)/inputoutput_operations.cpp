ebf345b7b12917515f60dd7f1dd9a74f112d2183
shivanialane <93445839+shivanialane@users.noreply.github.com>
2022-02-04 17:36:19 +0530

    Add files via upload

---
+++ C++ Lab/Experiment 14(a)/inputoutput_operations.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char x;
+    cout<<"Enter the character:";
+    cin.get(x);
+    cout<<"The given character is:";
+    cout.put(x);
+    cout<<endl;
+    cout<<"Printing n characters from given sentence:";
+    cout.write("Welcome to Hyderabad", 5);
+    cout<<endl;
+
+}
+
